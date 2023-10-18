a5fa95ba591a25c4325328e87bfdb8dc0ab8b773
roypromax <roy19rishabh@gmail.com>
2023-10-18 16:26:28 +0530

    Display List component

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Task from "./Task";
+import styles from "./List.module.css";
 
-const List = () => {
-  return <div>List</div>;
+const List = ({ listTitle, tasks }) => {
+  return (
+    <div className={styles.list}>
+      <h3>{listTitle}</h3>
+      <div className={styles.tasks}>
+        {tasks.map((task) => {
+          return <Task key={task.id} taskTitle={task.title} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default List;
