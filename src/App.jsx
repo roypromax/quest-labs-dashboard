099dc34db362b5f74a4fbd087b63677009a0976c
roypromax <roy19rishabh@gmail.com>
2023-10-18 15:04:48 +0530

    Display Board component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Board from "./components/Board";
+import dummyData from "./data/dummyData";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      {dummyData.map((data) => (
+        <Board key={data.id} boardTitle={data.boardTitle} lists={data.lists} />
+      ))}
+    </div>
+  );
 };
 
 export default App;
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styles from "./Board.module.css";
+import List from "./List";
 
-const Board = () => {
-  return <div>Board</div>;
+const Board = ({ boardTitle, lists }) => {
+  return (
+    <div className={styles.board}>
+      <h2>{boardTitle}</h2>
+      <div className={styles.lists}>
+        {lists.map((list) => {
+          return (
+            <List key={list.id} listTitle={list.title} tasks={list.tasks} />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Board;
